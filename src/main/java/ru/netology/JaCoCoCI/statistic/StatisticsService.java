0c39cc43c100e88cd01f55e84b5019175496cfe7
Lev <03011984lev@gmail.com>
2023-11-21 16:21:47 +0300

    Initial commit

---
+++ src/main/java/ru/netology/JaCoCoCI/statistic/StatisticsService.java
@@ -0,0 +1,14 @@
+package ru.netology.JaCoCoCI.statistic;
+
+    public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
+
