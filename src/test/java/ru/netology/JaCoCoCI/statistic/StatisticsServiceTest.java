cd4a2d89263eccee7f0c9cc382db3ca5a8d340c8
Lev <03011984lev@gmail.com>
2023-11-21 18:10:27 +0300

    Initial commit

--- src/test/java/ru/netology/JaCoCoCI/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/JaCoCoCI/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
